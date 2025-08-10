8072df1a271ae70908808c30be74f7fd4ed6c790
Arunabh Chetia <arunabhchetia44@gmail.com>
2025-08-10 12:39:30 +0530

    added sys path

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,4 +1,6 @@
 import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))  # noqa: E402
 sys.path.append("..")  # noqa: E402
 
 from fastapi.testclient import TestClient  # noqa: E402
