b99c1ccbfba118a6844f91bd4e363f2b0e4c1ab8
Arunabh Chetia <arunabhchetia44@gmail.com>
2025-08-10 15:31:56 +0530

    lint fix

--- app/main.py
+++ app/main.py
@@ -14,17 +14,10 @@ app = FastAPI()
 
 mlflow_tracking_uri = os.getenv("MLFLOW_TRACKING_URI", "databricks")
 mlflow.set_tracking_uri(mlflow_tracking_uri)
-
 client = MlflowClient()
-
 model_name = "mlops.default.california_housing_best_model"
-
-# Load model by alias "production" using '@' syntax 
 model = mlflow.sklearn.load_model(f"models:/{model_name}@production")
-
-# To get model version info, list all versions and filter manually
 all_versions = client.search_model_versions(f"name='{model_name}'")
-
 prod_version_info = None
 for v in all_versions:
     # Fetch full model version info
@@ -32,20 +25,14 @@ for v in all_versions:
     if 'production' in full_version.aliases:
         prod_version_info = full_version
         break
-
 if prod_version_info is None:
     raise Exception("Production alias not found for model")
 
 run_id = prod_version_info.run_id
-# Download artifacts associated with this run
 scaler_path = artifacts.download_artifacts(f"runs:/{run_id}/scaler.pkl")
 feature_columns_path = artifacts.download_artifacts(f"runs:/{run_id}/feature_columns.pkl")
-
 scaler = pickle.load(open(scaler_path, "rb"))
 feature_columns = pickle.load(open(feature_columns_path, "rb"))
-
-
-# Logging
 
 # Initialize SQLite connection and create table
 conn = sqlite3.connect('logs.db', check_same_thread=False)
@@ -59,6 +46,7 @@ CREATE TABLE IF NOT EXISTS logs (
 )
 ''')
 conn.commit()
+
 
 @app.post("/predict")
 async def predict(input_data: dict):
