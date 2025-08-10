1ae4208694289619a8a3bd9eee01b36126a54c47
Arunabh Chetia <arunabhchetia44@gmail.com>
2025-08-10 15:18:50 +0530

    added env variables

--- deploy.sh
+++ deploy.sh
@@ -4,11 +4,17 @@ set -e
 IMAGE="${DOCKERHUB_USERNAME}/housing-api:latest"
 
 echo "Pulling latest image..."
-docker pull $IMAGE
+docker pull "$IMAGE"
 
 echo "Stopping old container..."
 docker stop housing-api || true
 docker rm housing-api || true
 
 echo "Starting new container..."
-docker run -d -p 8080:8080 --name housing-api $IMAGE
+docker run -d -p 8080:8080 \
+  -e MLFLOW_TRACKING_URI="$MLFLOW_TRACKING_URI" \
+  -e DATABRICKS_HOST="$DATABRICKS_HOST" \
+  -e DATABRICKS_TOKEN="$DATABRICKS_TOKEN" \
+  --name housing-api "$IMAGE"
+
+echo "Done!"
